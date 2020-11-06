6bc4de5de16d23de02a478a11da38bcf0bd0a238
Albert Solana <iamtxena@gmail.com>
2020-11-06 09:24:55 +0100

    chore: fix configuration

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "validatedid-did-auth.js",
+    filename: "[name].js",
     library: "@validatedid/did-auth",
     libraryTarget: "umd",
     umdNamedDefine: true,
