6f2394b63a8d6f57d0ee175d5218af192571dafe
Albert Solana <iamtxena@gmail.com>
2020-11-18 11:36:06 +0100

    tests: added siop uri response tests

---
+++ tests/unit/SiopDidAuth.response.spec.ts
@@ -0,0 +1,206 @@
+import { parse } from "querystring";
+import {
+  createUriResponse,
+  DidAuthErrors,
+  DidAuthTypes,
+  DidAuthUtil,
+} from "../../src";
+import { mockedKeyAndDid } from "../AuxTest";
+
+describe("SiopDidAuth creat Uri Response tests should", () => {
+  it("throw an error BAD_PARAMS when no opts is passed", async () => {
+    expect.assertions(1);
+    await expect(createUriResponse(undefined as never)).rejects.toThrow(
+      DidAuthErrors.BAD_PARAMS
+    );
+  });
+
+  it("throw an error BAD_PARAMS when no opts.redirectUri is passed", async () => {
+    expect.assertions(1);
+    const opts = {};
+    await expect(createUriResponse(opts as never)).rejects.toThrow(
+      DidAuthErrors.BAD_PARAMS
+    );
+  });
+
+  it("throw an error BAD_PARAMS when no opts.signatureType is passed", async () => {
+    expect.assertions(1);
+    const opts = {
+      redirectUri: "https://entity.example/demo",
+    };
+    await expect(createUriResponse(opts as never)).rejects.toThrow(
+      DidAuthErrors.BAD_PARAMS
+    );
+  });
+
+  it("throw an error BAD_PARAMS when no opts.state is passed", async () => {
+    expect.assertions(1);
+    const { hexPrivateKey, did } = mockedKeyAndDid();
+    const opts = {
+      redirectUri: "https://entity.example/demo",
+      signatureType: {
+        hexPrivateKey,
+        did,
+        kid: `${did}#key-1`,
+      },
+    };
+    await expect(createUriResponse(opts as never)).rejects.toThrow(
+      DidAuthErrors.BAD_PARAMS
+    );
+  });
+
+  it("throw an error BAD_PARAMS when no opts.nonce is passed", async () => {
+    expect.assertions(1);
+    const { hexPrivateKey, did } = mockedKeyAndDid();
+    const state = DidAuthUtil.getState();
+    const opts = {
+      redirectUri: "https://entity.example/demo",
+      signatureType: {
+        hexPrivateKey,
+        did,
+        kid: `${did}#key-1`,
+      },
+      state,
+    };
+    await expect(createUriResponse(opts as never)).rejects.toThrow(
+      DidAuthErrors.BAD_PARAMS
+    );
+  });
+  it("throw an error BAD_PARAMS when no opts.registrationType is passed", async () => {
+    expect.assertions(1);
+    const { hexPrivateKey, did } = mockedKeyAndDid();
+    const state = DidAuthUtil.getState();
+    const opts = {
+      redirectUri: "https://entity.example/demo",
+      signatureType: {
+        hexPrivateKey,
+        did,
+        kid: `${did}#key-1`,
+      },
+      state,
+      nonce: DidAuthUtil.getNonce(state),
+    };
+    await expect(createUriResponse(opts as never)).rejects.toThrow(
+      DidAuthErrors.BAD_PARAMS
+    );
+  });
+
+  it("return a uriResponse when no response_mode is passed", async () => {
+    expect.assertions(9);
+    const { hexPrivateKey, did } = mockedKeyAndDid();
+    const state = DidAuthUtil.getState();
+    const opts: DidAuthTypes.DidAuthResponseOpts = {
+      redirectUri: "https://entity.example/demo",
+      signatureType: {
+        hexPrivateKey,
+        did,
+        kid: `${did}#key-1`,
+      },
+      state,
+      nonce: DidAuthUtil.getNonce(state),
+      registrationType: {
+        type: DidAuthTypes.ObjectPassedBy.VALUE,
+      },
+      did,
+    };
+
+    const uriResponse = await createUriResponse(opts);
+    expect(uriResponse).toBeDefined();
+    expect(uriResponse).toHaveProperty("urlEncoded");
+    expect(uriResponse).toHaveProperty("encoding");
+    expect(uriResponse).toHaveProperty("response_mode");
+    expect(uriResponse.encoding).toStrictEqual(
+      DidAuthTypes.UrlEncodingFormat.FORM_URL_ENCODED
+    );
+    expect(uriResponse.response_mode).toStrictEqual(
+      DidAuthTypes.DidAuthResponseMode.FRAGMENT
+    );
+    const uriResponseDecoded = decodeURIComponent(uriResponse.urlEncoded);
+    const splitUrl = uriResponseDecoded.split("#");
+    const responseData = parse(splitUrl[1]);
+    expect(responseData.id_token).toBeDefined();
+    expect(responseData.state).toBeDefined();
+    expect(responseData.state).toStrictEqual(state);
+  });
+  it("return a uriResponse with fragment when response_mode=fragment is passed", async () => {
+    expect.assertions(3);
+    const { hexPrivateKey, did } = mockedKeyAndDid();
+    const state = DidAuthUtil.getState();
+    const opts: DidAuthTypes.DidAuthResponseOpts = {
+      redirectUri: "https://entity.example/demo",
+      signatureType: {
+        hexPrivateKey,
+        did,
+        kid: `${did}#key-1`,
+      },
+      state,
+      nonce: DidAuthUtil.getNonce(state),
+      responseMode: DidAuthTypes.DidAuthResponseMode.FRAGMENT,
+      registrationType: {
+        type: DidAuthTypes.ObjectPassedBy.VALUE,
+      },
+      did,
+    };
+
+    const uriResponse = await createUriResponse(opts);
+    expect(uriResponse).toBeDefined();
+    expect(uriResponse).toHaveProperty("response_mode");
+    expect(uriResponse.response_mode).toStrictEqual(
+      DidAuthTypes.DidAuthResponseMode.FRAGMENT
+    );
+  });
+  it("return a uriResponse with query when response_mode=query is passed", async () => {
+    expect.assertions(3);
+    const { hexPrivateKey, did } = mockedKeyAndDid();
+    const state = DidAuthUtil.getState();
+    const opts: DidAuthTypes.DidAuthResponseOpts = {
+      redirectUri: "https://entity.example/demo",
+      signatureType: {
+        hexPrivateKey,
+        did,
+        kid: `${did}#key-1`,
+      },
+      state,
+      nonce: DidAuthUtil.getNonce(state),
+      responseMode: DidAuthTypes.DidAuthResponseMode.QUERY,
+      registrationType: {
+        type: DidAuthTypes.ObjectPassedBy.VALUE,
+      },
+      did,
+    };
+
+    const uriResponse = await createUriResponse(opts);
+    expect(uriResponse).toBeDefined();
+    expect(uriResponse).toHaveProperty("response_mode");
+    expect(uriResponse.response_mode).toStrictEqual(
+      DidAuthTypes.DidAuthResponseMode.QUERY
+    );
+  });
+  it("return a uriResponse with form_post when response_mode=form_post is passed", async () => {
+    expect.assertions(4);
+    const { hexPrivateKey, did } = mockedKeyAndDid();
+    const state = DidAuthUtil.getState();
+    const opts: DidAuthTypes.DidAuthResponseOpts = {
+      redirectUri: "https://entity.example/demo",
+      signatureType: {
+        hexPrivateKey,
+        did,
+        kid: `${did}#key-1`,
+      },
+      state,
+      nonce: DidAuthUtil.getNonce(state),
+      responseMode: DidAuthTypes.DidAuthResponseMode.FORM_POST,
+      registrationType: {
+        type: DidAuthTypes.ObjectPassedBy.VALUE,
+      },
+      did,
+    };
+    const uriResponse = await createUriResponse(opts);
+    expect(uriResponse).toBeDefined();
+    expect(uriResponse).toHaveProperty("response_mode");
+    expect(uriResponse.response_mode).toStrictEqual(
+      DidAuthTypes.DidAuthResponseMode.FORM_POST
+    );
+    expect(uriResponse).toHaveProperty("bodyEncoded");
+  });
+});
