059c86a8674bf3d553821cc44488d38c2fe61215
Albert Solana <iamtxena@gmail.com>
2020-11-25 17:22:19 +0100

    test: passed web2app tests

--- tests/e2e/SiopDidAuth.web2appFlow.spec.ts
+++ tests/e2e/SiopDidAuth.web2appFlow.spec.ts
@@ -87,6 +87,7 @@ describe("SIOP DID Auth end to end flow tests should", () => {
       verificationType: {
         registry: process.env.DID_REGISTRY_SC_ADDRESS,
         rpcUrl: process.env.DID_PROVIDER_RPC_URL,
+        didUrlResolver: `${WALLET_API_BASE_URL}/api/v1/identifiers`,
       },
     };
     const validationRequestResponse = await verifyDidAuthRequest(
@@ -162,8 +163,10 @@ describe("SIOP DID Auth end to end flow tests should", () => {
       verificationType: {
         verifyUri: `${WALLET_API_BASE_URL}/api/v1/signature-validations`,
         authZToken,
+        didUrlResolver: `${WALLET_API_BASE_URL}/api/v1/identifiers`,
       },
       nonce: (payload as DidAuthTypes.DidAuthResponsePayload).nonce,
+      redirectUri: validationRequestResponse.payload.client_id as string,
     };
     const validationResponse = await verifyDidAuthResponse(
       authResponseToken,
@@ -233,6 +236,7 @@ describe("SIOP DID Auth end to end flow tests should", () => {
       verificationType: {
         registry: process.env.DID_REGISTRY_SC_ADDRESS,
         rpcUrl: process.env.DID_PROVIDER_RPC_URL,
+        didUrlResolver: `${WALLET_API_BASE_URL}/api/v1/identifiers`,
       },
     };
     const validationRequestResponse = await verifyDidAuthRequest(
@@ -305,8 +309,10 @@ describe("SIOP DID Auth end to end flow tests should", () => {
       verificationType: {
         verifyUri: `${WALLET_API_BASE_URL}/api/v1/signature-validations`,
         authZToken,
+        didUrlResolver: `${WALLET_API_BASE_URL}/api/v1/identifiers`,
       },
       nonce: (payload as DidAuthTypes.DidAuthResponsePayload).nonce,
+      redirectUri: validationRequestResponse.payload.client_id as string,
     };
     const validationResponse = await verifyDidAuthResponse(
       authResponseToken,
